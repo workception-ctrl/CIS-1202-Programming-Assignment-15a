8d47792e9f31ec746db38a3e3824d3e173472193
workception-ctrl <workception@gmail.com>
2026-04-27 18:34:21 -0400

    Add project files.

---
+++ CIS 1202 Programming Assignment #15a/source.cpp
@@ -0,0 +1,17 @@
+//Tyler Brown
+//CIS 1202 501
+//April 27, 2026
+
+#include <iostream>
+
+using namespace std;
+
+char character(char, int);
+
+int main()
+{
+}
+
+char character(char start, int offset)
+{
+}
